7dfedd5b684be79587916a9830c49d652b96f436
Ektamadaan <ektamadaan45@gmail.com>
2024-02-13 23:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-class Person{
-	constructor(name , age){
-		this._name = name ;
-		this._age = age ;
-	}
-	get name(){
-		return this._name ;
-	}
-	get age(){
-		return this._age ;
-	}
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
-class Employee extends Person{
-	constructor(name, age, jobTitle) {
+
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
     super(name, age);
     this.jobTitle = jobTitle;
   }
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
 }
 
 // Do not change code below this line
